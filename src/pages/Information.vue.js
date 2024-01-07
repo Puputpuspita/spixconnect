c77121c85d862ce628e93ae3d9b72386208fcbe4
Puput Puspita <puputpuspita@student.telkomuniversity.ac.id>
2024-01-07 20:14:59 +0700

    maps img center

--- src/pages/Information.vue.js
+++ src/pages/Information.vue.js
@@ -1,33 +1,35 @@
 const Information = {
     template: `
     <div class="content-information container my-5">
-        <div class="content-information-details d-flex flex-column justify-content-center align-items-center">
+        <div class="d-flex flex-column justify-content-center align-items-center">
           <div class="title">
             <h1 class="text-center">INFORMATION</h1>
             <h1 class="text-center">MAPS & RUNDOWN</h1>
           </div>
-          <div class="title mt-5">
+          <div class="content-information-detail">
             <div class="mb-4">
               <h2>MAPS</h2>
-              <h2>UMKM, TECHNOLOGY, CONFERENCE</h2>
-              <img src="./src/assets/ASSET-04.png" class="img-fluid img-maps-conference" alt="maps-conference">
+              <h2 class="mb-3">UMKM, TECHNOLOGY, CONFERENCE</h2>
+              <div class="d-flex justify-content-center">
+                <img src="./src/assets/ASSET-04.png" class="img-fluid" alt="maps-conference">
+              </div>
             </div>
-            <div class="my-5">
+            <div class="mb-4">
               <h2>TENANTS</h2>
               <h2>INFORMATION</h2>
-              <img src="./src/assets/ASSET-05.png" class="img-fluid img-maps-tenant" alt="maps-tenant">
+              <img src="./src/assets/ASSET-05.png" class="img-fluid" alt="maps-tenant">
+              </div>
+            <div class="mb-4">
+              <img src="./src/assets/ASSET-06.png" class="img-fluid" alt="rundown-conference">
             </div>
-            <div class="my-5">
-              <img src="./src/assets/ASSET-06.png" class="img-fluid img-rundown-conference" alt="rundown-conference">
+            <div class="">
+              <img src="./src/assets/ASSET-07.png" class="img-fluid" alt="maps-gate">
+            </div>
+            <div class="">
+              <img src="./src/assets/ASSET-08.png" class="img-fluid" alt="rundown-festival">
             </div>
           </div>
             
-          <div class="my-5">
-            <img src="./src/assets/ASSET-07.png" class="img-fluid img-maps-gate" alt="maps-gate">
-          </div>
-          <div class="my-5">
-            <img src="./src/assets/ASSET-08.png" class="img-fluid img-rundown-festival" alt="rundown-festival">
-          </div>
 
         </div>
     </div>
