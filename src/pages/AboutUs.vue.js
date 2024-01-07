5de84ded0e4b506fce5db7c4f091efa318034915
Puput Puspita <puputpuspita@student.telkomuniversity.ac.id>
2024-01-07 18:37:41 +0700

    fix footer column

--- src/pages/AboutUs.vue.js
+++ src/pages/AboutUs.vue.js
@@ -1,8 +1,8 @@
 const About = {
     template: `
-    <div class="content-about container my-5">
-        <h1 class="">ABOUT</h1>
-        <h1 class="">SPIX</h1>
+    <div class="content-about container py-5">
+        <h1>ABOUT</h1>
+        <h1>SPIX</h1>
         <p class="mt-4">Spix Connect merupakan sebuah inisiatif akar rumput berbasis event, yang dibuat dengan misi utama untuk memfasilitasi jaringan antar ide-ide, panggung kesempatan, dan juga fitur-fitur spesial nan berpengaruh. Hal ini sejalan dengan motto dari Spix Connect yaitu "connecting the dots".</p>
     </div>
     `
--- src/pages/Home.vue.js
+++ src/pages/Home.vue.js
@@ -1,7 +1,7 @@
 const Home = {
   template: `
-    <div class="mx-4 my-5">
-        <img src="./src/assets/ASSET-03.png" class="img-fluid header-img" alt="header-spix">
+    <div class="mx-4 my-5 d-flex align-items-center justify-content-center">
+        <img src="./src/assets/ASSET-03.png" class="img-fluid header-img mx-auto" alt="header-spix">
     </div>
   `
 };
