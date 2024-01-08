9efc10c2e45146a1e8971f0b4520ed639f3c96ec
Puput Puspita <puputpuspita@student.telkomuniversity.ac.id>
2024-01-08 12:19:33 +0700

    navbar close responsive

--- src/pages/Home.vue.js
+++ src/pages/Home.vue.js
@@ -1,6 +1,6 @@
 const Home = {
   template: `
-    <div class="mx-4 my-5 d-flex align-items-center justify-content-center">
+    <div class="content-home mx-4 d-flex align-items-center justify-content-center">
         <img src="./src/assets/ASSET-03.png" class="img-fluid header-img mx-auto" alt="header-spix">
     </div>
   `
