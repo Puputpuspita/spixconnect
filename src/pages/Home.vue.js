7b605b98ffc390cfd9a72616b3932774b09e670f
Puput Puspita <puputpuspita@student.telkomuniversity.ac.id>
2024-01-12 16:34:02 +0700

    revisi layout

--- src/pages/Home.vue.js
+++ src/pages/Home.vue.js
@@ -5,8 +5,15 @@ const Home = {
     </div>
 
     <div class="content-about container py-5 d-flex flex-column justify-content-center align-items-center">
-        <h1>ABOUT SPIX</h1>
-        <p class="mt-4">Spix Connect merupakan sebuah inisiatif akar rumput berbasis event, yang dibuat dengan misi utama untuk memfasilitasi jaringan antar ide-ide, panggung kesempatan, dan juga fitur-fitur spesial nan berpengaruh. Hal ini sejalan dengan motto dari Spix Connect yaitu "connecting the dots".</p>
+        <div class="my-5">
+          <h2>ABOUT SPIX</h2>
+          <p class="mt-4">Spix Connect merupakan sebuah inisiatif akar rumput berbasis event, yang dibuat dengan misi utama untuk memfasilitasi jaringan antar ide-ide, panggung kesempatan, dan juga fitur-fitur spesial nan berpengaruh. Hal ini sejalan dengan motto dari Spix Connect yaitu "connecting the dots".</p>
+        </div>
+        <div class="my-5">
+          <h2>EXECUTIVE SUMMARY</h2>
+          <p class="mt-4">Inisiatif ini diorganisir oleh para pemuda yang ingin berdampak bagi sesamanya. Pada Acara kedua ini, kami berusaha untuk mewadahi UMKM terpilih untuk menjangkau pasar sasarannya dengan lebih masif. Kami juga bermaksud untuk menghadirkan panel diskusi dari figur-figur berdampak demi satu tujuan: gotong-royong meraih Indonesia Emas 2045.</p>
+          <p>Tak lupa, acara ini juga akan dihadiri oleh seniman-seniman apik yang kerap mewakili keresahan hati para Millenials dan juga Gen Z. Dengan mengusung tema “Indonesia Unggul,” besar keinginan kami untuk menjadikan acara ini sebagai katalis besar bagi kejayaan Indonesia. Terutama, dalam menyadarkan dan menggairahkan muda-mudi sejawat untuk terus berkarya demi Indonesia Emas 2045.</p>
+        </div>
     </div>
   `
 };
--- src/pages/Maps.vue.js
+++ src/pages/Maps.vue.js
@@ -1,28 +1,19 @@
 const Maps = {
     template: `
-    <div class="content-information container my-5">
+    <div class="content-information container">
         <div class="d-flex flex-column justify-content-center align-items-center">
           <div class="content-information-detail">
-            <div>
-                <h2>Maps</h2>
-                <h2 class="mb-3">Festival</h2>
-                <img src="./src/assets/ASSET-07.png" class="img-fluid" alt="maps-gate">
+            <div div class="img-container">
+              <h3>MAPS</h3>
+              <h3 class="mb-3">UMKM, TECHNOLOGY, CONFERENCE</h3>
+              <img src="./src/assets/ASSET-04.png" class="img-fluid" alt="maps-conference">
             </div>
-            <div>
-              <h2>Maps</h2>
-              <h2 class="mb-3">UMKM, Technology, Conference</h2>
-              <div class="d-flex justify-content-center">
-                <img src="./src/assets/ASSET-04.png" class="img-fluid" alt="maps-conference">
-              </div>
+            <div div class="img-container">
+              <h3>FESTIVAL</h3>
+              <h3 class="mb-3">MAPS</h3>
+              <img src="./src/assets/ASSET-07.png" class="img-fluid" alt="maps-gate"
             </div>
-            <div>
-              <h2>Tenants</h2>
-              <h2>Information</h2>
-              <img src="./src/assets/ASSET-05.png" class="img-fluid maps-tenant" alt="maps-tenant">
             </div>
-          </div>
-            
-
         </div>
     </div>
     `
